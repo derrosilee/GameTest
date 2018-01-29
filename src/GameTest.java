0b23ee12069efe4f207740d09e596fe1835a7ba6
eu-vinnom <vinnom.2d@gmail.com>
2018-01-29 12:45:23 -0200

    change code for character's name verification

--- src/GameTest.java
+++ src/GameTest.java
@@ -5,41 +5,23 @@ public class GameTest
 	public static void main( String[] args )
 	{
 		int control = 0;
+		String readName;
 		String readLine;
 
 		Knight knight = new Knight();
 
 		Scanner input = new Scanner( System.in );
 
-		System.out.println( "Type your character name:" );
-		readLine = input.nextLine();
-		knight.setCharacterName( readLine );
-		System.out.println( "\nAre you sure? (yes or no)" );
-		readLine = input.nextLine();
-		
-		while( control != 1 )
+		do
 		{
-			switch( readLine )
-			{
-				case "yes" :
-					System.out.println( "\nThen your name is: " + knight.getCharacterName() );
-					control = 1;
-					break;
-
-				default :
-					System.out.println( "\nType your character name:" );
-					readLine = input.nextLine();
-					knight.setCharacterName( readLine );
-					System.out.println( "\nAre you sure? (yes or no)" );
-					readLine = input.nextLine();
-					if( readLine.equals( "yes" ) )
-					{
-						System.out.println( "\nThen your name is: " + knight.getCharacterName() );
-						control = 1;
-					}
-					break;
-			}
+			System.out.println( "Type your character name:" );
+			readName = input.nextLine();
+			knight.setCharacterName( readName );
+			System.out.println( "\nAre you sure? (yes or no)" );
+			readLine = input.nextLine();
+			System.out.println(  );
 		}
+		while( readLine.equals( "no" ) );
 
 	}
 }
