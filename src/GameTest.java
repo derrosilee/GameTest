0f294a490c8fbac501417cdc7a96d28de553f3c2
eu-vinnom <vinnom.2d@gmail.com>
2018-02-06 16:19:23 -0200

    modified so new classes could be tested

--- src/GameTest.java
+++ src/GameTest.java
@@ -2,19 +2,22 @@ import java.util.Scanner;
 
 public class GameTest
 {
+	private static Scanner input;
+
 	public static void main( String[] args )
 	{
-		int control = 0;
 		String readName;
 		String readLine;
 
-		Knight knight = new Knight();
+		Character knight = new Knight();
+		Character mage = new Mage();
+		Character archer = new Archer();
 
-		Scanner input = new Scanner( System.in );
+		input = new Scanner( System.in );
 
 		do
 		{
-			System.out.println( "Type your character name:" );
+			System.out.println( "Type your knight character name:" );
 			readName = input.nextLine();
 			knight.setCharacterName( readName );
 			System.out.println( "\nAre you sure? (yes or no)" );
@@ -23,13 +26,51 @@ public class GameTest
 		}
 		while( readLine.equals( "no" ) );
 
-		System.out.println( "Now, lets generate your character stats." );
+		System.out.printf( "\n\nNow, lets generate %s character stats.\n", knight.characterName );
 		knight.setClassAtributes();
 		knight.printStats();
 		System.out.println();
 		knight.generateStats();
 		System.out.println();
 		knight.printStats();
+
+		do
+		{
+			System.out.println( "Type your mage character name:" );
+			readName = input.nextLine();
+			mage.setCharacterName( readName );
+			System.out.println( "\nAre you sure? (yes or no)" );
+			readLine = input.nextLine();
+			System.out.println();
+		}
+		while( readLine.equals( "no" ) );
+
+		System.out.printf( "\n\nNow, lets generate %s character stats.\n", mage.characterName );
+		mage.setClassAtributes();
+		mage.printStats();
+		System.out.println();
+		mage.generateStats();
+		System.out.println();
+		mage.printStats();
+
+		do
+		{
+			System.out.println( "Type your archer character name:" );
+			readName = input.nextLine();
+			archer.setCharacterName( readName );
+			System.out.println( "\nAre you sure? (yes or no)" );
+			readLine = input.nextLine();
+			System.out.println();
+		}
+		while( readLine.equals( "no" ) );
+
+		System.out.printf( "\n\nNow, lets generate %s character stats.\n", archer.characterName );
+		archer.setClassAtributes();
+		archer.printStats();
+		System.out.println();
+		archer.generateStats();
+		System.out.println();
+		archer.printStats();
 
 	}
 }
