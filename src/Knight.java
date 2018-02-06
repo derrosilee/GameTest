31347eaa9fd593f10d9af5f2119438cb2f675551
eu-vinnom <vinnom.2d@gmail.com>
2018-02-06 16:18:49 -0200

    add new field className and removed generateStats method

--- src/Knight.java
+++ src/Knight.java
@@ -4,48 +4,28 @@ public class Knight extends Character
 	@Override
 	public void setClassAtributes()
 	{
+		this.className = "Knight";
 		this.strenght = 5;
 		this.dexterity = 3;
 		this.intelligence = 2;
 	}
-	
+
 	@Override
 	public void printStats()
 	{
+		if( strenght == 5 && dexterity == 3 && intelligence == 2 )
 		{
-			if( strenght == 5 && dexterity == 3 && intelligence == 2 )
-			{
-				System.out.println( "==========Knight basic stats:" );
-				System.out.printf( "Strenght: %5d\n", this.strenght );
-				System.out.printf( "Dexterity: %4d\n", this.dexterity );
-				System.out.printf( "Inteligence: %2d\n", this.intelligence );
-			}
-			else
-			{
-				System.out.println( "==========Knight new stats:" );
-				System.out.printf( "Strenght: %5d\n", this.strenght );
-				System.out.printf( "Dexterity: %4d\n", this.dexterity );
-				System.out.printf( "Inteligence: %2d\n", this.intelligence );
-			}
+			System.out.printf( "==========%s basic stats:\n", this.className );
+			System.out.printf( "Strenght: %5d\n", this.strenght );
+			System.out.printf( "Dexterity: %4d\n", this.dexterity );
+			System.out.printf( "Inteligence: %2d\n", this.intelligence );
 		}
-
-	}
-
-	@Override
-	public void generateStats()
-	{
-		int d6;
-
-		System.out.println( "A six faces dice will be used to generate your new stats\n" );
-		System.out.println( "==========Generating new stats:" );
-		d6 = 1 + randomDice.nextInt( 6 );
-		this.strenght += d6;
-		System.out.printf( "Strenght     +%d\n", d6 );
-		d6 = 1 + randomDice.nextInt( 6 );
-		this.dexterity += d6;
-		System.out.printf( "Dexterity    +%d\n", d6 );
-		d6 = 1 + randomDice.nextInt( 6 );
-		this.intelligence += d6;
-		System.out.printf( "Intelligence +%d\n", d6 );
+		else
+		{
+			System.out.printf( "==========%s new stats:\n", this.className );
+			System.out.printf( "Strenght: %5d\n", this.strenght );
+			System.out.printf( "Dexterity: %4d\n", this.dexterity );
+			System.out.printf( "Inteligence: %2d\n", this.intelligence );
+		}
 	}
 }
